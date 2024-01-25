0a411136ce9779bb1dcf0c30aeffb0f35b92e6f1
Nakamura-Takaaki0607 <k022g0031@g.neec.ac.jp>
2024-01-25 14:35:59 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.cpp
@@ -0,0 +1,146 @@
+﻿#include "Player.h"
+#include "ImGuiManager.h"
+#include "MathUtilityForText.h"
+#include "Vector3.h" 
+#include <cassert>
+
+void Player::Initialize(Model* modelBody, Model* modelHead, Model* modelL_arm, Model* modelR_arm) {
+	// NULLポインタチェック
+	assert(modelBody);
+	assert(modelHead);
+	assert(modelL_arm);
+	assert(modelR_arm);
+
+	// メンバ変数に受け取った値を代入
+	modelBody_ = modelBody;
+	modelHead_ = modelHead;
+	modelL_arm_ = modelL_arm;
+	modelR_arm_ = modelR_arm;
+
+	// ワールド変換の初期化
+	worldTransformBase_.Initialize();
+
+	worldTransformBody_.Initialize();
+	worldTransformBody_.parent_ = &worldTransformBase_;
+
+	worldTransformHead_.Initialize();
+	worldTransformHead_.parent_ = &worldTransformBody_;
+	worldTransformHead_.translation_.y = 1.5038f;
+
+	worldTransformL_arm_.Initialize();
+	worldTransformL_arm_.parent_ = &worldTransformBody_;
+	worldTransformL_arm_.translation_.x = -0.5275f;
+	worldTransformL_arm_.translation_.y = 1.2619f;
+
+	worldTransformR_arm_.Initialize();
+	worldTransformR_arm_.parent_ = &worldTransformBody_;
+	worldTransformR_arm_.translation_.x = +0.5275f;
+	worldTransformR_arm_.translation_.y = 1.2619f;
+
+	// 浮遊ギミックの初期化
+	InitializeFloatingGimmick();
+
+	// インプット
+	input_ = Input::GetInstance();
+}
+
+void Player::Update() {
+
+	// 浮遊ギミックの更新
+	UpdateFloatingGimmick();
+
+	const float speed = 0.3f;
+	//const float threshold = 0.5f;
+	//bool isMoving = false;
+
+	Vector3 move = {0.0f, 0.0f, 0.0f};
+	if (input_->PushKey(DIK_RIGHT)) {
+		move.x = 1.0f;
+	}
+	if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+		move.x = -1.0f;
+	}
+	if (Input::GetInstance()->PushKey(DIK_UP)) {
+		move.z = 1.0f;
+	}
+	if (Input::GetInstance()->PushKey(DIK_DOWN)) {
+		move.z = -1.0f;
+	}
+
+	move = Normalize(move) * speed;
+
+	if (viewProjection_) {
+		// カメラの回転行列
+		Matrix4x4 matRotX = MakeRotateXMatrix(viewProjection_->rotation_.x);
+		Matrix4x4 matRotY = MakeRotateYMatrix(viewProjection_->rotation_.y);
+		Matrix4x4 matRotZ = MakeRotateZMatrix(viewProjection_->rotation_.z);
+		// 回転行列の合成
+		Matrix4x4 matRot = matRotZ * matRotX * matRotY;
+
+		// 移動量をカメラの回転に合わせて回転させる
+		move = TransformNormal(move, matRot);
+	}
+
+	worldTransformBase_.translation_ += move;
+
+	// 移動ベクトルのY軸周り角度
+	worldTransformBase_.rotation_.y = std::atan2(move.x, move.z);
+
+	// 変換行列を更新
+	worldTransformBase_.UpdateMatrix();
+	worldTransformBody_.UpdateMatrix();
+	worldTransformHead_.UpdateMatrix();
+	worldTransformL_arm_.UpdateMatrix();
+	worldTransformR_arm_.UpdateMatrix();
+
+	/*worldTransformBase_.matWorld_ = MakeAffineMatrix(
+	    worldTransformBase_.scale_, worldTransformBase_.rotation_,
+	    worldTransformBase_.translation_);*/
+	// 変換行列を定数バッファに転送
+	//worldTransformBase_.TransferMatrix();
+
+	/*ImGui::Begin("Player");
+	ImGui::SliderFloat3(
+	    "Head Translation", reinterpret_cast<float*>(&worldTransformHead_.translation_), -10.0f,
+	    10.0f);
+	ImGui::SliderFloat3(
+	    "ArmL Translation", reinterpret_cast<float*>(&worldTransformL_arm_.translation_), -10.0f,
+	    10.0f);
+	ImGui::SliderFloat3(
+	    "ArmR Translation", reinterpret_cast<float*>(&worldTransformR_arm_.translation_), -10.0f,
+	    10.0f);
+	ImGui::SliderInt("floatingCycle", reinterpret_cast<int*>(&floatingCycle_), 1, 200);
+	ImGui::SliderFloat("floatingAmplitude", &floatingAmplitude_, 0.0f, 10.0f);
+	ImGui::SliderFloat("idleArmAngleMax_", &idleArmAngleMax_, 0.0f, 180.0f);
+	ImGui::End();*/
+}
+
+void Player::Draw(const ViewProjection& viewProjection) {
+	// 3Dモデルを描画
+	modelBody_->Draw(worldTransformBody_, viewProjection);
+	modelHead_->Draw(worldTransformHead_, viewProjection);
+	modelL_arm_->Draw(worldTransformL_arm_, viewProjection);
+	modelR_arm_->Draw(worldTransformR_arm_, viewProjection);
+}
+
+void Player::InitializeFloatingGimmick() { floatingParameter_ = 0.0f; }
+
+void Player::UpdateFloatingGimmick() {
+
+	floatingParameter_ += 0.1f;
+	worldTransformBody_.translation_.y = std::sin(floatingParameter_) * 0.2f;
+	worldTransformL_arm_.rotation_.x = std::sin(floatingParameter_) * 0.5f;
+	worldTransformR_arm_.rotation_.x = std::sin(floatingParameter_) * 0.5f;
+
+	//const float floatingStep = PI * 2.0f / floatingCycle_;
+
+	//// 浮遊ギミックを更新
+	//floatingParameter_ += floatingStep;
+	//// 2πを超えたら0に戻す
+	//floatingParameter_ = fmodf(floatingParameter_, PI * 2.0f);
+	//// 浮遊を座標に反映
+	//worldTransformBody_.translation_.y = std::sin(floatingParameter_) * floatingAmplitude_;
+
+	//worldTransformL_arm_.rotation_.x = std::sin(floatingParameter_) * idleArmAngleMax_ / 180.0f * PI;
+	//worldTransformR_arm_.rotation_.x = std::sin(floatingParameter_) * idleArmAngleMax_ / 180.0f * PI;
+}
---
+++ scene/GameScene.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+// #include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+#include "AxisIndicator.h"
+#include "DebugCamera.h"
+#include "FollowCamera.h"
+#include "Ground.h"
+#include "Player.h"
+#include "Skydome.h"
+#include <memory>
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+
+class GameScene {
+
+public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+	// 3Dモデル
+	std::unique_ptr<Model> modelCube_;
+	std::unique_ptr<Model> modelSkydome_;
+	std::unique_ptr<Model> modelGround_;
+	std::unique_ptr<Model> modelFighterBody_;
+	std::unique_ptr<Model> modelFighterHead_;
+	std::unique_ptr<Model> modelFighterL_arm_;
+	std::unique_ptr<Model> modelFighterR_arm_;
+
+	// クラス
+	// std::unique_ptr<Model> model_;
+
+	// 自キャラ
+	std::unique_ptr<Player> player_;
+	// 天球
+	std::unique_ptr<Skydome> skydome_;
+	// 地面
+	std::unique_ptr<Ground> ground_;
+
+	// デバッグカメラ
+	std::unique_ptr<DebugCamera> debugCamera_;
+	// デバッグカメラ有効
+	bool isDebugCameraActive_ = false;
+
+	// 追従カメラ
+	std::unique_ptr<FollowCamera> followCamera_;
+};
